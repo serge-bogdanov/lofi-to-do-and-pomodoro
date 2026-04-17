d7dac37ee92b78fc9a69628c821ad3ee1b346578
serge-bogdanov <si000bi01@gmail.com>
2026-04-17 16:58:16 +0500

    feat: добавлена js логика

--- src/script.js
+++ src/script.js
@@ -1,9 +1,39 @@
-const form = document.querySelector(".todo-form")
-const list = document.querySelector(".todo__list") 
+const form = document.querySelector(".todo-form");
+const list = document.querySelector(".todo__list");
 
-form.addEventListener("submit", addItem)
+form.addEventListener("submit", addItem);
 
 function addItem(event) {
-    event.preventDefault()
-    console.log("ee")
+  event.preventDefault();
+  let formInput = document.querySelector(".todo-form__input");
+  let inputText = formInput.value;
+
+  if (inputText !== "") {
+    let item = document.createElement("li");
+    item.className = "todo__list-item";
+
+    let textNode = document.createTextNode(inputText);
+    item.appendChild(textNode);
+
+    let deleteButton = document.createElement("button");
+    deleteButton.appendChild(document.createTextNode("X"));
+    deleteButton.className = "todo-item__button";
+    deleteButton.dataset.action = "delete";
+    item.appendChild(deleteButton);
+
+    list.prepend(item);
+
+    formInput.value = "";
+  }
+}
+
+list.addEventListener("click", deleteItem);
+
+function deleteItem(event) {
+  if (
+    event.target.hasAttribute("data-action") &&
+    event.target.getAttribute("data-action") === "delete"
+  ) {
+    event.target.parentNode.remove();
+  }
 }
