3f723592cb968478ccadccecca35f75b5a672227
serge-bogdanov <si000bi01@gmail.com>
2026-04-18 15:22:27 +0500

    feat: добавлено сохрание в localStorage

--- src/script.js
+++ src/script.js
@@ -3,25 +3,54 @@ const list = document.querySelector(".todo__list");
 
 form.addEventListener("submit", addItem);
 
+function getStorageTodos() {
+  let storage = localStorage.getItem("todos");
+  if (storage === null) {
+    return [];
+  }
+  return JSON.parse(storage);
+}
+
+function saveTodos(todos) {
+  let jsonStorage = JSON.stringify(todos);
+  localStorage.setItem("todos", jsonStorage);
+}
+
+function loadTodos() {
+  let todos = getStorageTodos();
+  for (const todo of todos) {
+    list.append(createTodoElement(todo))
+  }
+}
+
+function createTodoElement(text) {
+  let item = document.createElement("li");
+  item.className = "todo__list-item";
+
+  let textNode = document.createTextNode(text);
+  item.appendChild(textNode);
+
+  let deleteButton = document.createElement("button");
+  deleteButton.appendChild(document.createTextNode("X"));
+  deleteButton.className = "todo-item__button";
+  deleteButton.dataset.action = "delete";
+  item.appendChild(deleteButton)
+
+  return item;
+}
+
+
 function addItem(event) {
   event.preventDefault();
   let formInput = document.querySelector(".todo-form__input");
   let inputText = formInput.value;
 
   if (inputText !== "") {
-    let item = document.createElement("li");
-    item.className = "todo__list-item";
-
-    let textNode = document.createTextNode(inputText);
-    item.appendChild(textNode);
-
-    let deleteButton = document.createElement("button");
-    deleteButton.appendChild(document.createTextNode("X"));
-    deleteButton.className = "todo-item__button";
-    deleteButton.dataset.action = "delete";
-    item.appendChild(deleteButton);
-
-    list.prepend(item);
+    list.prepend(createTodoElement(inputText));
+    
+    let todos = getStorageTodos();
+    todos.push(inputText);
+    saveTodos(todos);
 
     formInput.value = "";
   }
@@ -35,5 +64,7 @@ function deleteItem(event) {
     event.target.getAttribute("data-action") === "delete"
   ) {
     event.target.parentNode.remove();
+
   }
+
 }
